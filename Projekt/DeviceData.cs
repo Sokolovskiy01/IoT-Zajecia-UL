a10676289d6ec3ef4421afce4cdd66f8861caef1
Dmytro Sokolovskyi <Sokolovskiy01@gmail.com>
2022-12-21 19:20:43 +0100

    Initial commit

---
+++ Projekt/DeviceData.cs
@@ -0,0 +1,27 @@
+﻿using Opc.UaFx;
+
+class DeviceData
+{
+    OpcNodeId nodeId { get; }
+
+    public Int32 ProductionStatus { get; set; } = 0;
+    public String WorkorderId { get; set; } = string.Empty;
+
+    private Int32 _productionRate;
+    public Int32 ProductionRate { get { return _productionRate; }
+        set {
+            if (value >= 0 && value <= 100) _productionRate = value;
+            else throw new ArgumentOutOfRangeException();
+        }
+    }
+    public Int64 GoodCount { get; set; } = 0;
+    public Int64 BadCount { get; set; } = 0;
+    public Double Temperature { get; set; } = 0.0;
+    public Int32 DeviceError { get; set; } = 0;
+
+    public DeviceData(OpcNodeId nodeId)
+    {
+        this.nodeId = nodeId;
+    }
+
+}
---
+++ Projekt/Program.cs
@@ -0,0 +1,47 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Client;
+
+List<DeviceData> deviceDatas = new List<DeviceData>();
+
+using (var client = new OpcClient("opc.tcp://localhost:4840/"))
+{
+    try
+    {
+        client.Connect();
+
+        // we will have 
+
+
+        OpcNodeInfo serverNodes = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+
+        foreach (var device in serverNodes.Children())
+        {
+            string nodeName = device.DisplayName.ToString();
+            if (nodeName.Contains("Device"))
+            {
+                // Device found
+                //devicesList.Add(client.BrowseNode(device.NodeId));
+                
+                DeviceData newDevice = new DeviceData(device.NodeId);
+                OpcValue ProductionStatus = client.ReadNode(device.NodeId + "/ProductionStatus");
+
+                deviceDatas.Add(newDevice);
+            }
+        }
+
+        while(true)
+        {
+            Console.WriteLine('.');
+
+
+
+            Thread.Sleep(5000);
+        }
+
+        client.Disconnect();
+    }
+    catch (OpcException opcex)
+    {
+        Console.WriteLine(opcex.Message);
+    }
+}
